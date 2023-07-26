f0909decde0d0b1d072790f17ac49b1b3ce4894b
Syedmunirullah12 <140606948+Syedmunirullah12@users.noreply.github.com>
2023-07-26 17:25:27 +0500

    Assign-4.ts

---
+++ Assign-4.ts
@@ -0,0 +1,5 @@
+// Famous Quote by Albert Einstein
+const quote = `"A person who never made a mistake never tried anything new."`;
+const authorName = `Albert Einstein`;
+
+console.log(`${authorName} once said, ${quote}`);

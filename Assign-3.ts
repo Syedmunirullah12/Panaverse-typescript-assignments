870e080370575f83f85761d6caa06b49fa05d6ee
Syedmunirullah12 <140606948+Syedmunirullah12@users.noreply.github.com>
2023-07-26 17:23:21 +0500

    Assign-3.ts

---
+++ Assign-3.ts
@@ -0,0 +1,10 @@
+let fullName = "Munir Ullah";
+
+// Uppercase in typescript
+console.log(fullName.toUpperCase());
+
+// Lowercase in typescript
+console.log(fullName.toLowerCase());
+
+// Titlecase in typescript
+console.log(`${fullName.charAt(0).toUpperCase()} ${fullName.slice(1)}`)

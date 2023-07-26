42c1a58181f88e397a95e547a7b15d0334716d21
Syedmunirullah12 <140606948+Syedmunirullah12@users.noreply.github.com>
2023-07-26 17:38:39 +0500

    Assign-10.ts

---
+++ Assign-10.ts
@@ -0,0 +1,12 @@
+// Store a persons name in a variable and print a message to that person
+
+let fullName = "Munir Ullah"
+console.log(`Hello ${fullName}, would you like to learn some JavaScript Today?`);
+
+// Famous Quote by Albert Einstein
+const author = `Albert Einstein`;
+const quote = `"A person who never made a mistake did not tried anything new"`;
+
+console.log(`${author} once said, ${quote}`);
+
+`This tasks shows us how we can print a message who wrote that message`;

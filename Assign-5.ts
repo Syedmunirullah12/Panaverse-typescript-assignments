f4584636ba5dd27e9d2db50600e95a8090ca8033
Syedmunirullah12 <140606948+Syedmunirullah12@users.noreply.github.com>
2023-07-26 17:29:48 +0500

    Assign-5.ts

---
+++ Assign-5.ts
@@ -0,0 +1,5 @@
+let famous_person = `Albeert Einstein`;
+let famous_Quote = `"A person who never made a mistake never tried anything new"`;
+let message = `${famous_person} once said, ${famous_Quote}`;
+
+console.log(message);
